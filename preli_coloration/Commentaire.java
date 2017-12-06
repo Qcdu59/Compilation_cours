8908f4ad934b49c822b0b52dad3409dae89f90bb
qcoloos <quentin.coloos@gmail.com>
2017-12-06 11:59:18 +0100

    commit

--- preli_coloration/Commentaire.java
+++ preli_coloration/Commentaire.java
@@ -1,18 +1,17 @@
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Commentaire {
-    public static void main(String [] args) throws IOException {
-	System.out.println(args[0]);
+	public static void main(String [] args) throws IOException {
+		System.out.println(args[0]);
 
-	EnleveCommentaires yy = null;
-	yy = new EnleveCommentaires(new FileReader(args[0]));
+		EnleveCommentaires yy = null;
+		yy = new EnleveCommentaires(new FileReader(args[0]));
 
-	Yytoken token;
+		Yytoken token;
 
-	while((token = yy.yylex()) != null) {
-	    System.out.print(token.description.toUpperCase());
+		while((token = yy.yylex()) != null) {
+			System.out.print(token.description.toUpperCase());
+		}
 	}
-    }
 }
